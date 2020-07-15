00482bd881c0637b379579a1c05ab1d1ef661994
Nestor Andres Pena <nestor2509@gmail.com>
2020-07-15 18:21:20 -0500

    campo alfanumerico

--- index.php
+++ index.php
@@ -171,7 +171,7 @@ foreach($preregistro as $item):
                     $resp .= $l;
                 endif;
 
-                $resp .= sprintf('%10d', $postID);
+                $resp .= sprintf('%010d', $postID);
 
                 echo $resp;
             ?>
